e197618b9f8c9d3e96550d20107d1103b7b6f128
mkuhikar <mkuhikar@gmail.com>
2025-05-04 16:12:13 +0530

    #PV1

--- app/main.go
+++ app/main.go
@@ -42,14 +42,15 @@ func main() {
 	\n adds a newline after the output. */
 
 	correlation_id := binary.BigEndian.Uint32(buffer[8:12]) //converted 4 bytes of buffer to uInt32, in big endian most significant byte comes first
-	message_size := binary.BigEndian.Uint32(buffer[0:4])    //converted 4 bytes of buffer to uInt32, in big endian most significant byte comes first
-	response := make([]byte, 10)
+	message_size := uint32(16 - 4)                          //It should be the number of bytes in your response body + header, excluding the first 4 bytes (the message_size field itself).
+	response := make([]byte, 16)
 	binary.BigEndian.PutUint32(response[0:4], message_size)
 	binary.BigEndian.PutUint32(response[4:8], correlation_id) //pass uint32 correlation id , correlation id consits of 4 byte, each byte is of 2 hex digits
 	binary.BigEndian.PutUint16(response[8:10], 0)
-	binary.BigEndian.PutUint16(response[10:12], 18)
-	binary.BigEndian.PutUint16(response[12:14], 0)
-	binary.BigEndian.PutUint16(response[14:16], 4)
+	binary.BigEndian.PutUint32(response[10:14], 1)
+	binary.BigEndian.PutUint16(response[14:16], 18)
+	response = append(response, 0x00, 0x00) // MinVersion = 0
+	response = append(response, 0x00, 0x04) // MaxVersion = 4
 	// response:= []byte{0,0,0,0,0,0,0,7} // just a hard coded way to send correlation id
 	fmt.Println(response)
 
